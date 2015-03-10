baa45c53f5bee6b41a6f00599b9677fd90cd5d02
Darren Miller <darren@dmlogic.com>
2015-03-10 13:06:40 +0000

    Initial commit

---
+++ dbslave.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Usage
+# Login as root
+# ./provision.sh local_mysql_password
+
+# Update
+apt-get update
+
+# MySQL root password from input
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $1"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $1"
+
+apt-get install -qq mysql-server
+
+printf '\n%s' 'server-id = 2' >> /etc/mysql/my.cnf
+printf '\n%s' 'log-bin = /var/lib/mysql/bin-log' >> /etc/mysql/my.cnf
+printf '\n%s' 'log-slave-updates = 1' >> /etc/mysql/my.cnf
+
+service mysql restart
+
+mysql -u root -p$1 -e 'CREATE DATABASE performapp';
